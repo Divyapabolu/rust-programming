2cd04bd3c5aeb6a7811ac24cc68840ccd54da727
Divyapabolu <22pa1a05b7@vishnu.edu.in>
2025-07-16 11:48:45 +0530

    Create 3.rs

---
+++ unit-3/3.rs
@@ -0,0 +1,35 @@
+/*
+3. Write a program to make a calculator using Match Expression
+*/
+
+fn main() {
+    let a = 5;
+    let b = 7;
+    let operator = '*';
+    match operator {
+        '+' => {
+            println!("{}", a+b);
+        },
+        '-' => {
+            println!("{}", a-b);
+        },
+        '*' => {
+            println!("{}", a*b);
+        },
+        '/' => {
+            if b == 0 {
+                println!("Division by 0 is undefined");
+            }
+            else {
+                println!("{}", a/b);
+            }
+        },
+        '%' => {
+            println!("{}", a%b);
+        }
+        _ => {
+            println!("invalid operator");
+        }
+    }
+} 
+    
