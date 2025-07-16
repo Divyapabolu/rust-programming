df7bf85b31a3e267facdf22919126b28e3e14486
Divyapabolu <22pa1a05b7@vishnu.edu.in>
2025-07-16 11:19:08 +0530

    Create 1.rs

---
+++ 1.rs
@@ -0,0 +1,10 @@
+/*
+1. Write a program to implement Type Casting Operator.
+*/
+
+fn main() {
+    let f: i32 = 24;
+    let t = (f as f64)/3.0;
+    println!("f = {}", f);
+    println!("t = {}", t);
+}
